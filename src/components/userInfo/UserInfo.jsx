a88909daec955b0bc89dcf2b443858205d1b4db2
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-20 18:09:48 -0600

    agregando la estructura para mostrar la informacion del usuario foto, nombre, y 3 icons para agregar video, editar o ver mas

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -1,7 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const UserInfo = () => {
   return (
-    <div>
-      <h1>User Info</h1>
+    <div className="userInfo">
+      <div className="user">
+        <img src="https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="pic avatar user" />
+        <h2>Mabi Lina</h2>
+      </div>
+      <div className="icons">
+        <FontAwesomeIcon icon="fa-solid fa-ellipsis" />
+        <FontAwesomeIcon icon="fa-solid fa-video" />
+        <FontAwesomeIcon icon="fa-solid fa-pen-to-square" />
+      </div>
     </div>
   )
 }
