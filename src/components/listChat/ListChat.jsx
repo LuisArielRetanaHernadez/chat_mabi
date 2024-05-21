4b42914bb88562d4fca392b6503aebda9ced52e5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-20 20:48:23 -0600

    agregando la estructura para crear el buscador de chats

--- src/components/listChat/ListChat.jsx
+++ src/components/listChat/ListChat.jsx
@@ -1,7 +1,17 @@
+// font-awesome
+import { faCirclePlus, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const ListChat = () => {
   return (
-    <div>
-      ListChat
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input type="text" />
+        </div>
+        <FontAwesomeIcon icon={faCirclePlus} />
+      </div>
     </div>
   )
 }
