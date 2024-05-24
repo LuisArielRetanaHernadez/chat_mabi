7ba3124f044efe742b0fb2057aff7858a49d1c67
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-23 19:43:52 -0600

    creando la estructura del user en la seccion top

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,7 +3,14 @@ import './chat.style.css'
 const Chat = () => {
   return (
     <div className='chat'>
-      <div className='top'></div>
+      <div className='top'>
+        <div className='user'>
+          <img src='https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
+          <span>Ahmad</span>
+          <p>lorem inpurt rtext inpurt rtext lorem inpurt rtext </p>
+        </div>
+        <div className='icons'></div>
+      </div>
       <div className='center'></div>
       <div className='bottom'></div>
     </div>
