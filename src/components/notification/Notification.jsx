e4d7ceda61301eff11039cec9fc754217f880915
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-25 15:54:46 -0600

    creando el componente notification y la hoja de estilos notification.style.css

---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,12 @@
+import 'notification.style.css'
+
+const Notification = () => {
+  return (
+    <div className="notification">
+      <button>Dismiss</button>
+      <h1>You have a new message</h1>
+    </div>
+  )
+}
+
+export default Notification
