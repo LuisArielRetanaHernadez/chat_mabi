737440b939782e72976dbc8c7086781dab6e63ba
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-24 20:22:58 -0600

    restando la estructura con el proyecto inicial y los estilos

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -4,70 +4,98 @@ import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 const Detail = () => {
   return (
-    <div className='detail'>
-      <div className='detail__user'>
-        <img className='detail__user-avatar-image' />
-        <h2>mabi Godzilla</h2>
-        <p>Mamadisima</p>
+    <div className="detail">
+      <div className="user">
+        <img src="" alt="" />
+        <h2>mabigod</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
       </div>
-      <div className='detail__information'>
-        <div className='detail__option'>
-          <div className='detail__title'>
-            <span>Chat Setting</span>
-            <img alt='' />
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
           </div>
         </div>
-        <div className='detail__option'>
-          <div className='detail__title'>
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
             <span>Privacy & help</span>
-            <img alt='' />
+            <img src="./arrowUp.png" alt="" />
           </div>
         </div>
-        <div className='detail__option'>
-          <div className='detail__title'>
-            <span>Shered photos</span>
-            <img alt='' />
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
           </div>
-          <div className='photos'>
-            <div className='photos__item'>
-              <div className='photos__photo-detial'>
-                <img alt='' />
-                <span>photo_11-11-1.png</span>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
               </div>
-              <span className='photo__icon-download'>
+              <span className='icon'>
                 <FontAwesomeIcon icon={faFileArrowDown} />
               </span>
             </div>
-
-            <div className='photos__item'>
-              <div className='photos__photo-detial'>
-                <img alt='' />
-                <span>photo_11-11-1.png</span>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
               </div>
-              <span className='photo__icon-download'>
+              <span className='icon'>
                 <FontAwesomeIcon icon={faFileArrowDown} />
               </span>
             </div>
-
-            <div className='photos__item'>
-              <div className='photos__photo-detial'>
-                <img alt='' />
-                <span>photo_11-11-1.png</span>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
               </div>
-              <span className='photo__icon-download'>
+              <span className='icon'>
                 <FontAwesomeIcon icon={faFileArrowDown} />
               </span>
             </div>
-
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <span className='icon'>
+                <FontAwesomeIcon icon={faFileArrowDown} />
+              </span>
+            </div>
           </div>
         </div>
-        <div className='detail__option'>
-          <div className='detail__title'>
-            <span>Share Files</span>
-            <img alt='' />
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
           </div>
         </div>
-        <button>Block User</button>
+        <button >
+        </button>
+        <button className="logout" >
+          Logout
+        </button>
       </div>
     </div>
   )
