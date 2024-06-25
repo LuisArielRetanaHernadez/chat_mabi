8c9b74a561383b90c1068a582cb35cb5d9652b22
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-24 18:39:27 -0600

    creando la estructura del componente Detail.jsx

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,7 +3,19 @@ import './detail.style.css'
 const Detail = () => {
   return (
     <div className='detail'>
-      <h1>Detail</h1>
+      <div className='detail__user'>
+        <img className='detail__user' />
+        <h2>mabi Godzilla</h2>
+        <p>Mamadisima</p>
+      </div>
+      <div className='detail__information'>
+        <div className='detail__option'>
+          <div className='detail__title'>
+            <span>Chat Setting</span>
+            <img alt='' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
