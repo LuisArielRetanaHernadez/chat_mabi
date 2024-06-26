74179ffd12d28a438c4b0a94c15c89dc70d55390
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-25 22:10:32 -0600

    retornando el valor downloadURL en la subida del archivo

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -79,7 +79,7 @@ const Login = () => {
         // Handle successful uploads on complete
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log('File available at', downloadURL);
+          return downloadURL;
         });
       }
     );
