043a7e283e8176fa9fc482bb673186f70425f13f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-25 21:51:31 -0600

    creando la funcion uploadImageProfile con el codigo para subir la foto el usuario cuando se registra

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,9 +2,10 @@ import './login.style.css'
 import { useState } from 'react'
 
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth, db } from '../lib/firebase'
+import { auth, db, storage } from '../lib/firebase'
 import { doc, setDoc } from "firebase/firestore";
 import { toast } from 'react-toastify';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 
 
 const Login = () => {
@@ -36,6 +37,52 @@ const Login = () => {
   const handleLogin = (e) => {
     e.preventDefault()
     console.log('Login')
+  }
+
+  const uploadImageProfile = async () => {
+    // const storageRef = ref(storage, `images/${avatar.file.name}`);
+    // const uploadTask = uploadBytesResumable(storageRef, avatar.file);
+    // await uploadTask;
+    // const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+    // return downloadURL;
+
+
+    const nameFile = new Date().getTime() + avatar.file.name;
+    const storageRef = ref(storage, `images/${nameFile}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, avatar.file);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log(error)
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
   }
 
   const handleRegister = async (e) => {
