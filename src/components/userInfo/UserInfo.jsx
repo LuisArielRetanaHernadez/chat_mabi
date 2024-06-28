b0c312b8f778bd4c237010109aecee976b6ff9cc
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-27 20:56:31 -0600

    mostrando la informacion del usuario logeado

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -2,13 +2,15 @@ import { faEllipsis, faPenToSquare, faVideo } from '@fortawesome/free-solid-svg-
 import './userInfo.style.css'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useStore } from '../../lib/userStorage'
 
 const UserInfo = () => {
+  const { currentUser } = useStore()
   return (
     <div className="userInfo">
       <div className="user">
-        <img src="https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="pic avatar user" />
-        <h2 className='user__name'>Mabi Lina</h2>
+        <img src={`${currentUser.avatar || 'https://images.pexels.com/photos/1302436/pexels-photo-1302436.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}`} alt="pic avatar user" />
+        <h2 className='user__name'>{currentUser.username || 'nunidefine'}</h2>
       </div>
       <div className="icons">
         <span className='icon'>
