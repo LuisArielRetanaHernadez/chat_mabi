8f512cede4e5e83faa46699b159518699b733147
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-27 22:37:00 -0600

    iterando los chats que tiene el usuario

--- src/components/listChat/ListChat.jsx
+++ src/components/listChat/ListChat.jsx
@@ -38,27 +38,15 @@ const ListChat = () => {
           <FontAwesomeIcon icon={faCirclePlus} />
         </span>
       </div>
-      <div className="item">
-        <img src="https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <div className="texts">
-          <span>Mabi Bella</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <div className="texts">
-          <span>Mabi Bella</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <div className="texts">
-          <span>Mabi Bella</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {chats.map((chat, index) => (
+        <div className="item" key={index}>
+          <img src={chat.avatar} />
+          <div className="texts">
+            <span>{chat.username}</span>
+            <p>{chat.lastMessage}</p>
+          </div>
+        </div> || <span>Sin chats</span>
+      ))}
     </div>
   )
 }
