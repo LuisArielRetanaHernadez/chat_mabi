29c83ccae593028b35f867191133bb81f13b1682
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-28 16:49:13 -0600

    creando el archivo useChatStore con la funcion useChatStore

---
+++ src/lib/useChatStore.js
@@ -0,0 +1,26 @@
+import { create } from 'zustand'
+import { useStore } from './userStorage'
+
+export const useChatStore = create((set) => ({
+  user: null,
+  isCurrentUserBlocked: false,
+  isReceiverBlocked: false,
+  changeChat: async (chatId, user) => {
+    const currentUser = useStore.getState().currentUser
+
+
+    if (currentUser.blocked.includes(user.id)) {
+      return set({
+        chatId,
+        user,
+        isCurrentUserBlocked: false,
+        isReceiverBlocked: true
+      })
+    }
+  },
+  changeBlock: () => {
+    set((state) => ({
+      ...state, isReceiverBlock: !state.isReceiverBlock
+    }))
+  }
+}))
