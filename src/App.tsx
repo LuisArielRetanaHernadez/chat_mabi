3cc40dfd58e354abb7d3a6c370cc4d13dba64525
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-27 20:47:37 -0600

    creando la condicional para autentificar el usuario para rederigirlo a la vista del chat si la autentificacion fue exitosa

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,35 @@ import "./App.css";
 import Chat from "./components/chat/Chat";
 import List from "./components/list/List";
 import Detail from "./components/detail/Detail";
-import { useState } from "react";
+import { useEffect } from "react";
 import Login from "./login/Login";
+import { useStore } from "./lib/userStorage";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 function App() {
-  const [isLogin, setisLogin] = useState(false);
+  const { currentUser, isLoading, fetchCurrentUser } = useStore();
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      fetchCurrentUser(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchCurrentUser]);
+  console.log(currentUser);
+
+  if (isLoading)
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
   return (
     <div className="container">
-      {isLogin ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
