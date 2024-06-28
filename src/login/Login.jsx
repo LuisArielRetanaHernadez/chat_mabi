645e30f437138a38e4c388c57723aaba0c8d7ce9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-27 19:30:00 -0600

    importando la funcion uploadFile para cargar la imagen del nuevo usuario

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -6,6 +6,7 @@ import { auth, db, storage } from '../lib/firebase'
 import { doc, setDoc } from "firebase/firestore";
 import { toast } from 'react-toastify';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import uploadFile from '../lib/uploadFile';
 
 
 const Login = () => {
@@ -44,45 +45,13 @@ const Login = () => {
     }
   }
 
-  const uploadImageProfile = async () => {
-    if (!avatar.file) return;
-    const nameFile = new Date().getTime() + avatar.file.name;
-    const storageRef = ref(storage, `images/${nameFile}`);
-
-    const uploadTask = uploadBytesResumable(storageRef, avatar.file);
-
-    return new Promise((resolve, reject) => {
-      uploadTask.on('state_changed',
-        (snapshot) => {
-          // const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-              break;
-            case 'running':
-              console.log('Upload is running');
-              break;
-          }
-        },
-        (error) => {
-          reject(error)
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL)
-          });
-        }
-      );
-    })
-  }
-
 
 
   const handleRegister = async (e) => {
     e.preventDefault()
     try {
       const resposne = await createUserWithEmailAndPassword(auth, dataRegister.email, dataRegister.password)
-      const urlImageProfile = await uploadImageProfile()
+      const urlImageProfile = await uploadFile(avatar.file, 'images')
       // Add a new document in collection "cities"
       await setDoc(doc(db, "users", resposne.user.uid), {
         ...dataRegister,
