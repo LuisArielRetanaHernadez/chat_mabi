d5566bd8fb0ba7c678fe48d40c7b270fdd39262b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-28 21:26:45 -0600

    corregiendo el argumento del id usario enla funcion changeChat

--- src/components/listChat/ListChat.jsx
+++ src/components/listChat/ListChat.jsx
@@ -22,22 +22,28 @@ const ListChat = () => {
     const unSub = onSnapshot(doc(db, "userChats", currentUser.id), async (res) => {
 
       const itemsChat = res.data().chats
+      console.log('itemsChat ', itemsChat)
+
+      // get user data
       const promisse = itemsChat.map(async (item) => {
         const userRef = doc(db, "users", item.receiverId)
         const userSnap = await getDoc(userRef)
 
         const user = userSnap.data()
 
-        return { ...user, lastMessage: item.lastMessage }
+        return { user, ...item }
       });
 
       const chatData = await Promise.all(promisse)
       console.log('chatData ', chatData)
-      setChats(chatData.sort((a, b) => {
-        if (a.lastMessage && b.lastMessage) {
-          return a.lastMessage.time - b.lastMessage.time
-        }
-      }))
+      // setChats(chatData.sort((a, b) => {
+      //   if (a.lastMessage && b.lastMessage) {
+      //     return a.lastMessage.time - b.lastMessage.time
+      //   }
+      // }))
+
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+
       console.log('chats ', chats)
 
     });
@@ -48,24 +54,31 @@ const ListChat = () => {
   }, [currentUser.id])
 
   const handleSelect = async (chat) => {
+    console.log('chat ', chat)
+
+    // update isSeen  
     const userChats = chats.map((item) => {
-      const { user, ...rest } = item;
+      const { ...rest } = item;
       return rest;
     });
+
+    console.log('userChats ', userChats)
 
     const chatIndex = userChats.findIndex(
       (item) => item.chatId === chat.chatId
     );
 
+    console.log('chatIndex ', chatIndex)
+
     userChats[chatIndex].isSeen = true;
 
-    const userChatsRef = doc(db, "userchats", currentUser.id);
-
+    const userChatsRef = doc(db, "userChats", currentUser.id);
+    console.log('userChatsRef ', userChatsRef)
     try {
       await updateDoc(userChatsRef, {
         chats: userChats,
       });
-      changeChat(chat.chatId, chat.user);
+      changeChat(chat.chatId, chat.user.id);
     } catch (err) {
       console.log(err);
     }
@@ -84,10 +97,10 @@ const ListChat = () => {
         </span>
       </div>
       {chats.map((chat) => (
-        <div className="item" key={chat.id} onClick={() => handleSelect}>
-          <img src={chat.avatar} />
+        <div className="item" key={chat.chatId} onClick={() => handleSelect(chat)}>
+          <img src={chat.user.avatar} />
           <div className="texts">
-            <span>{chat.username}</span>
+            <span>{chat.user.username}</span>
             <p>{chat.lastMessage}</p>
           </div>
         </div> || <span>Sin chats</span>
