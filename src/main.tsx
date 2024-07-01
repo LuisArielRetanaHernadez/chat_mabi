5a3266427064d6fe272c517d25a93b6610a954b3
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-01 16:02:06 -0600

    importando router dom en el main.tsx para configurar las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// router dom
+import { createRoot } from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
