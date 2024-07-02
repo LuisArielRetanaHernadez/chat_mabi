10640cd6e275a766b5e456c20ed77d7d7964cb07
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-01 19:33:27 -0600

    creando la estructura jsx del component page Auth

--- src/pages/Auth/Auth.page.jsx
+++ src/pages/Auth/Auth.page.jsx
@@ -1,8 +1,44 @@
 const Auth = () => {
   return (
-    <div>
-      <h1>Auth</h1>
-    </div>
+    <section>
+      <div>
+        <div>
+          <form>
+            <div>
+              <input type="email" placeholder='email' />
+            </div>
+            <div>
+              <input type="password" placeholder='pasword' />
+            </div>
+            <button>Entrar</button>
+          </form>
+        </div>
+        <div>
+          <form>
+            <div>
+              <img src="" alt="" />
+              <label htmlFor="image-profile">
+                Imagen
+              </label>
+              <input type="file" style={{ display: 'none' }} id="image-profile" />
+            </div>
+            <div>
+              <input placeholder='Username' />
+            </div>
+            <div>
+              <input type="email" placeholder='email' />
+            </div>
+            <div>
+              <input type="password" placeholder='pasword' />
+            </div>
+            <div>
+              <input type="password" placeholder='pasword confirm' />
+            </div>
+            <button>Unirse</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
