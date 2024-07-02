f5d17b53b5926cc66847b3d4a12956286402f1a0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-01 19:49:37 -0600

    importando la hoja de estilos del layout menu

--- src/layouts/MenuLayout/Menu.layout.jsx
+++ src/layouts/MenuLayout/Menu.layout.jsx
@@ -2,6 +2,8 @@ import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Outlet } from "react-router-dom";
 
+import './menu.style.css'
+
 const MenuLayout = () => {
   return (
     <section className="page">
