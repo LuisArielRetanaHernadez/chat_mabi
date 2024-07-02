bafe241f365090393e5683ae815ea99e4e3d73f5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-01 20:17:10 -0600

    creando el componente Logo con su hoja de estilos

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import './logo.style.css'
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="logo__text">
+        ChatMabi
+      </span>
+    </div>
+  )
+
+}
+
+export default Logo
