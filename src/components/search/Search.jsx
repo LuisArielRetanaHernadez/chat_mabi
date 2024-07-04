8b1772e093e2b8e9bdb33333ea42d663d011e596
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-03 22:12:42 -0600

    creando los estilos inciales del componente

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -7,7 +7,7 @@ const Search = ({ search, setSearch }) => {
   return (
     <div className='search'>
       <input className='search__input' type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
-      <span className='button button--search'>
+      <span className='button search__button'>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </span>
     </div>
