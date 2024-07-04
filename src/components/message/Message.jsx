6e0de789c3582c2b4746d43e0ed6373d16ccf262
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-04 17:11:24 -0600

    agregando contenido prev-eliminar

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,16 +1,22 @@
 
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 import './message.style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Message = () => {
   return (
     <div className="message">
-      <div className="message__avatar"></div>
+      <div className="message__avatar">
+        <img src='https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+      </div>
       <div className="message__message">
-        <div className="message__username"></div>
-        <div className="message__text"></div>
+        <div className="message__username">Username</div>
+        <div className="message__text">Prueba del componente Message del chat</div>
       </div>
       <div className="message__about">
-        <div className="message__time"></div>
-        <div className="message__status"></div>
+        <div className="message__time">1/11/111 - 1:11 PM</div>
+        <div className="message__status">
+          <FontAwesomeIcon icon={faCircleCheck} />
+        </div>
       </div>
 
     </div>
