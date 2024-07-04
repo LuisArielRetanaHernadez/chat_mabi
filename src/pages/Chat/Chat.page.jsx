bce9daed63b92997d5684519884dab2df7886ab5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-03 23:44:37 -0600

    creando el componente page chat y su hoja de estilos

---
+++ src/pages/Chat/Chat.page.jsx
@@ -0,0 +1,16 @@
+
+import './chat.style.css'
+const Chat = () => {
+  return (
+    <div className="chat">
+      <div className="chat__messages">
+        {/* box messages */}
+      </div>
+      <div className="chat__send-message">
+        {/* send message */}
+      </div>
+    </div>
+  )
+}
+
+export default Chat
