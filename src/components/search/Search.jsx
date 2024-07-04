20564649a7e045c4aeb3c751beb8a8d228156119
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-03 21:56:00 -0600

    creando el component Search

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,9 @@
+const Search = ({ search, setSearch }) => {
+  return (
+    <div>
+      <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+    </div>
+  )
+}
+
+export default Search
