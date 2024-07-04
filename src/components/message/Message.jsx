61131b10ce0ef44615c4ae7d130b111b856f56e8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-04 17:17:35 -0600

    agregando ancho y alto x2 al avatar de la image

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -6,7 +6,7 @@ const Message = () => {
   return (
     <div className="message">
       <div className="message__avatar">
-        <img src='https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+        <img className='message__avatar-image' src='https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
       </div>
       <div className="message__message">
         <div className="message__username">Username</div>
