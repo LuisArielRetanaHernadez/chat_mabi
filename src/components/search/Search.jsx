0c63db9b5501d988e369090d8d0eee220e23d0a0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-03 22:05:24 -0600

    creando el boton submit donde agregamos el icon lupa

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
 import './search.style.css'
 
 const Search = ({ search, setSearch }) => {
   return (
-    <div>
-      <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+    <div className='search'>
+      <input className='search__input' type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <span className='button button--search'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </span>
     </div>
   )
 }
