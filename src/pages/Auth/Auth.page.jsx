41a0129055c997c34fd0c64d309cfa0b26f8a194
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-05 17:02:26 -0600

    agregado el componente ToastContainer para mostrar notificiaciones de error o exito

--- src/pages/Auth/Auth.page.jsx
+++ src/pages/Auth/Auth.page.jsx
@@ -2,6 +2,10 @@ import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import './auth.style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useRef, useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../lib/firebase'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const Auth = () => {
   const [modeAuth, setModeAuth] = useState('login')
@@ -35,12 +39,37 @@ const Auth = () => {
       buttonNextRef.current.classList.add('auth__button-next--register')
     }
   }, [modeAuth])
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      await signInWithEmailAndPassword(auth, dataForm.email, dataForm.password)
+    } catch (error) {
+      // el parametro erro me arroja un strin pero quiero que sea un json o objeto 
+      const stringError = JSON.stringify(error)
+      const errorObject = JSON.parse(stringError)
+      if (errorObject.code === 'auth/invalid-credentia'
+        || errorObject.code === 'auth/invalid-email'
+        || errorObject.code === 'auth/invalid-password') {
+        toast.error('credenciales invalidas', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        })
+      }
+    }
+  }
   return (
     <section className="auth">
       <div className="auth__box">
         <div className="auth__login auth__section" ref={loginRef}>
           <h2 className="auth__title">Entra a chatear!</h2>
-          <form className="auth__form">
+          <form className="auth__form" onSubmit={handleLogin}>
             <div className="auth__form-field">
               <input className="auth__form-input" type="email" name="email" placeholder='email' onChange={e => setDataForm(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
             </div>
@@ -79,6 +108,8 @@ const Auth = () => {
         <span className='button auth__button-next' onClick={() => setModeAuth(prev => prev === 'login' ? 'register' : 'login')} ref={buttonNextRef}>
           {modeAuth === 'login' ? <FontAwesomeIcon icon={faArrowLeft} /> : <FontAwesomeIcon icon={faArrowRight} />}
         </span>
+        <ToastContainer />
+
       </div>
     </section>
   )
