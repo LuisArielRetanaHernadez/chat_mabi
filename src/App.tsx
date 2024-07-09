44f42b342cbd6a180aa86fd054519d39108abf50
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-09 15:32:39 -0600

    comantando el componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,49 @@
-import "./App.css";
+// import "./App.css";
 
-import Chat from "./components/chat/Chat";
-import List from "./components/list/List";
-import Detail from "./components/detail/Detail";
-import { useEffect } from "react";
-import Login from "./login/Login";
-import { useStore } from "./lib/userStorage";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./lib/firebase";
-import { useChatStore } from "./lib/useChatStore";
+// import Chat from "./components/chat/Chat";
+// import List from "./components/list/List";
+// import Detail from "./components/detail/Detail";
+// import { useEffect } from "react";
+// import Login from "./login/Login";
+// import { useStore } from "./lib/userStorage";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "./lib/firebase";
+// import { useChatStore } from "./lib/useChatStore";
 
-function App() {
-  const { currentUser, isLoading, fetchCurrentUser } = useStore();
-  const { chatId } = useChatStore();
-  useEffect(() => {
-    const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
-      fetchCurrentUser(user?.uid);
-    });
+// function App() {
+//   const { currentUser, isLoading, fetchCurrentUser } = useStore();
+//   const { chatId } = useChatStore();
+//   useEffect(() => {
+//     const unSub = onAuthStateChanged(auth, (user) => {
+//       console.log(user);
+//       fetchCurrentUser(user?.uid);
+//     });
 
-    return () => {
-      unSub();
-    };
-  }, [fetchCurrentUser]);
-  console.log(currentUser);
+//     return () => {
+//       unSub();
+//     };
+//   }, [fetchCurrentUser]);
+//   console.log(currentUser);
 
-  if (isLoading)
-    return (
-      <>
-        <h1>Loading...</h1>
-      </>
-    );
-  return (
-    <div className="App">
-      {currentUser ? (
-        <>
-          <List />
-          {chatId && <Chat />}
-          {chatId && <Detail />}
-        </>
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
-}
+//   if (isLoading)
+//     return (
+//       <>
+//         <h1>Loading...</h1>
+//       </>
+//     );
+//   return (
+//     <div className="App">
+//       {currentUser ? (
+//         <>
+//           <List />
+//           {chatId && <Chat />}
+//           {chatId && <Detail />}
+//         </>
+//       ) : (
+//         <Login />
+//       )}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
