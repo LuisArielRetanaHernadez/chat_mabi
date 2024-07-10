1a14ae5089c09f147b6574c12fdcdccf778a51ed
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-09 21:07:51 -0600

    creando la estructura Profile.jsx

--- src/pages/Profile/Profile.page.jsx
+++ src/pages/Profile/Profile.page.jsx
@@ -1,10 +1,63 @@
+import { Link } from 'react-router-dom';
 import './profile.style.css'
 
 const Profile = () => {
   return (
     <div>
-      <h2>Profile</h2>
-      <p>This is the Profile component.</p>
+      <div>
+        <img />
+      </div>
+      <div>
+        <p>Username</p>
+        <span>Conectados</span>
+      </div>
+      <ul>
+        <li>Nombre de usuario</li>
+        <li>Correo de usuario</li>
+      </ul>
+      <div>
+        <h2>conexiones de user 1</h2>
+        <span>Conexiones visbles</span>
+        <span>109 conexiones</span>
+        <ul>
+          <li>
+            <Link to="">
+              <div>
+                <img />
+              </div>
+              <div>
+                <p>Name</p>
+                <span>Contacto</span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div>
+                <img />
+              </div>
+              <div>
+                <p>Name</p>
+                <span>Hablamos?</span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              <div>
+                <img />
+              </div>
+              <div>
+                <p>Name</p>
+                <span>No me Hables</span>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button>Elimar cuenta</button>
+      </div>
     </div>
   );
 }
