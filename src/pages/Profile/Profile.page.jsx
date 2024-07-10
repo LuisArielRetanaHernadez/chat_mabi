2bf60e9318e9452c331253dd58dc764c47a7bda8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-09 19:54:29 -0600

    creando el componente page Profile y su hoja de estilos

---
+++ src/pages/Profile/Profile.page.jsx
@@ -0,0 +1,12 @@
+import './profile.style.css'
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>This is the Profile component.</p>
+    </div>
+  );
+}
+
+export default Profile;
